26afbae4bd03fff2ed8c0a09c9e0a9972e836619
Artiom <artiom.levitskiy@gmail.com>
2021-02-14 17:59:17 +0300

    add custom registrations, and view for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable,  :validatable
+  validates :last_name, presence: true
 end
