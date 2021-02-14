d8eda12e8ea874cda8dc418bfe7b4db520514b99
Artiom <artiom.levitskiy@gmail.com>
2021-02-15 01:58:07 +0300

    add comments, and privilege for admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'users/profile', as: 'user_root'
   root "tasks#index", as: 'home_page'
-  resources :tasks
-  # do
-  #   resources :users
-  # end
+  resources :tasks do
+    resources :comments
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_181242) do
+ActiveRecord::Schema.define(version: 2021_02_14_223108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -39,5 +48,6 @@ ActiveRecord::Schema.define(version: 2021_02_14_181242) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "tasks"
   add_foreign_key "tasks", "users"
 end
